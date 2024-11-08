2d2d6cf079553340870c6fc0a753b348363802e5
Daniel Zapata <zdaniel7697@gmail.com>
2024-11-08 14:47:01 -0800

    added some parsers to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,34 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"css",
-				"diff",
-				"html",
-				"javascript",
-				"lua",
-				"markdown",
-				"python",
-				"query",
-				"vimdoc",
-			},
-			sync_install = false,
-
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "bash",
+        "css",
+        "diff",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "php",
+        "python",
+        "query",
+        "sql",
+        "toml",
+        "typescript",
+        "vimdoc",
+        "yaml",
+      },
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
 }

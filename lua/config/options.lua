6d6bb88596374c28db2a8871e4463b580fdbba97
Daniel Zapata <zdaniel7697@gmail.com>
2024-11-21 18:57:09 -0800

    added scrollof option, set to 15

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,6 +11,7 @@ vim.opt.laststatus = 3
 vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 15
 vim.opt.shiftwidth = 2
 vim.opt.showmode = false
 vim.opt.signcolumn = "yes"

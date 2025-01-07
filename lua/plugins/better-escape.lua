38c3832c87bdfb2bce06d9833af04b0fdeeb482a
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-06 22:29:37 -0800

    betesc done

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,24 +1,30 @@
 return {
-	{
-		"max397574/better-escape.nvim",
-		config = function()
-			require("better_escape").setup({
-				default_mappings = false,
-				mappings = {
-					i = {
-						j = {
-							k = "<Esc>",
-							j = "<Esc>",
-						},
-					},
-					c = {
-						j = {
-							k = "<Esc>",
-							j = "<Esc>",
-						},
+	"max397574/better-escape.nvim",
+	opts = {
+		{
+			default_mappings = false,
+			mappings = {
+				i = {
+					j = {
+						k = "<Esc>",
 					},
 				},
-			})
-		end,
+				c = {
+					j = {
+						k = "<Esc>",
+					},
+				},
+				v = {
+					j = {
+						k = "<Esc>",
+					},
+				},
+				s = {
+					j = {
+						k = "<Esc>",
+					},
+				},
+			},
+		}
 	},
 }

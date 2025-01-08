1aca451e56cd19abf5ba171262ba9a74ba406d4c
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-07 16:48:44 -0800

    fixed betesc, cant use opts have to use config function and req setup

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,30 +1,25 @@
 return {
-	"max397574/better-escape.nvim",
-	opts = {
-		{
-			default_mappings = false,
-			mappings = {
-				i = {
-					j = {
-						k = "<Esc>",
-					},
-				},
-				c = {
-					j = {
-						k = "<Esc>",
-					},
-				},
-				v = {
-					j = {
-						k = "<Esc>",
-					},
-				},
-				s = {
-					j = {
-						k = "<Esc>",
-					},
-				},
-			},
-		}
-	},
+  "max397574/better-escape.nvim",
+  config = function()
+    require("better_escape").setup({
+      default_mappings = false,
+      mappings = {
+        i = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+        v = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+        s = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+      },
+    })
+  end,
 }

110f9c1c091acc614b003a5895dee5139012ece4
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-07 19:21:55 -0800

    added supertab to blink-cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,6 +3,29 @@ return {
   dependencies = "rafamadriz/friendly-snippets",
   version = "*",
   opts = {
-    keymap = { preset = "super-tab" },
+    keymap = {
+      preset = "none",
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.snippet_forward()
+          else
+            return cmp.accept()
+          end
+        end,
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.snippet_backward()
+          end
+          return false
+        end,
+        "fallback",
+      },
+    },
   },
 }

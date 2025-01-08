18ad0f103f6da97e2ea89c52c0a28431dbd15798
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-07 16:32:31 -0800

    done with gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,35 +1,51 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	opts = {
-		on_attach = function(buffer)
-			local gs = package.loaded.gitsigns
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "󰍵" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    signs_staged = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "󰍵" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+    },
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
 
-			local function map(mode, l, r, desc)
-				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-			end
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
 
-			map("n", "]h", function()
-				if vim.wo.diff then
-					vim.cmd.normal({ "]c", bang = true })
-				else
-					gs.nav_hunk("next")
-				end
-			end, "Next Hunk")
-			map("n", "[h", function()
-				if vim.wo.diff then
-					vim.cmd.normal({ "[c", bang = true })
-				else
-					gs.nav_hunk("prev")
-				end
-			end, "Prev Hunk")
+      map("n", "]h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
+        end
+      end, "Next Hunk")
 
-			map({ "n", "v" }, "<leader>gs", "<CMD>Gitsigns stage_hunk<CR>", "Stage Hunk")
-			map({ "n", "v" }, "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>", "Unstage Hunk")
-			map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
-			map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
-			map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
-			map("n", "<leader>gp", gs.preview_hunk_inline, "Preview Hunk Inline")
-			map("n", "<leader>gd", gs.diffthis, "Diff This")
-		end,
-	},
+      map("n", "[h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
+        end
+      end, "Prev Hunk")
+
+      map({ "n", "v" }, "<leader>gs", "<CMD>Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "v" }, "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>", "Unstage Hunk")
+      map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>gp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>gd", gs.diffthis, "Diff This")
+    end,
+  },
 }

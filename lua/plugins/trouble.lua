ba94d06433a0bd409c2d4d2daaafab88e6cb8da7
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-08 18:43:10 -0800

    added trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/trouble.nvim",
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+  },
+}

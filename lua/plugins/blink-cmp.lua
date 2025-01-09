ec948c72a996fa4e09af1dfdba6b3e8351262d10
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-08 21:26:28 -0800

    changed over to super-tab for blink, and added mini.pairs and mini.ai

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,28 +4,8 @@ return {
   version = "*",
   opts = {
     keymap = {
-      preset = "none",
-      ["<C-j>"] = { "select_next", "fallback" },
-      ["<C-k>"] = { "select_prev", "fallback" },
-      ["<Tab>"] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.snippet_forward()
-          else
-            return cmp.accept()
-          end
-        end,
-        "fallback",
-      },
-      ["<S-Tab>"] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.snippet_backward()
-          end
-          return false
-        end,
-        "fallback",
-      },
+      preset = "super-tab",
+      ["<C-y>"] = { "select_and_accept", "fallback" },
     },
   },
 }

ef5fbfb07b4da3b6f01b3f6513572d9ea5ed71dc
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-10 02:14:44 -0800

    changed up keymaps for scrolling through blink cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,8 +4,23 @@ return {
   version = "*",
   opts = {
     keymap = {
-      preset = "super-tab",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
       ["<C-y>"] = { "select_and_accept", "fallback" },
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then return cmp.accept()
+          else return cmp.select_and_accept() end
+        end,
+        "snippet_forward",
+        "fallback"
+      },
+
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<Up>"] = { "scroll_documentation_up", "fallback" },
+      ["<Down>"] = { "scroll_documentation_down", "fallback" },
     },
   },
 }

f48bc15973718189f39a1d04ff13771aee27703c
Daniel Zapata <zdaniel7697@gmail.com>
2025-01-10 19:45:28 -0800

    got rid of auto starting in insert mode for floating terminal

--- lua/utils/terminal.lua
+++ lua/utils/terminal.lua
@@ -44,8 +44,6 @@ M.toggle_terminal = function()
     if vim.bo[state.floating.buf].buftype ~= "terminal" then
       vim.cmd.terminal()
     end
-
-    vim.api.nvim_command("startinsert")
   else
     vim.api.nvim_win_hide(state.floating.win)
   end

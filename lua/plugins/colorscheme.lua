b645a37b7fffd676782775d6f2f903b21cf98dde
Daniel Zapata <zdaniel7697@gmail.com>
2025-02-26 18:53:26 -0800

    switched from everforest to nordic

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,14 +7,4 @@ return {
       require("nordic").load()
     end,
   },
-  -- {
-  --   "neanias/everforest-nvim",
-  --   version = false,
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("everforest").setup({})
-  --     vim.cmd.colorscheme("everforest")
-  --   end,
-  -- },
 }

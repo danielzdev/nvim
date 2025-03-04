8483c1b192307f1025d78fac7ffc26f547e75830
Daniel Zapata <zdaniel7697@gmail.com>
2025-03-04 09:17:46 -0800

    added oil and avante, trying them out

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,25 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false,
+  opts = {
+    provider = "claude",
+  },
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "ibhagwan/fzf-lua",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}

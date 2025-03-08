d95c6c1fd69583e403faf5174ced8549e6925044
Daniel Zapata <zdaniel7697@gmail.com>
2025-03-08 15:36:16 -0800

    got rid of java related plugins, not doing java stuff, rn

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -54,7 +54,6 @@ return {
 
     local servers = {
       clangd = {},
-      jdtls = {},
       pyright = {},
       zls = {},
       lua_ls = {
@@ -80,7 +79,6 @@ return {
     })
 
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-    require("java").setup()
     require("mason-lspconfig").setup({
       handlers = {
         function(server_name)

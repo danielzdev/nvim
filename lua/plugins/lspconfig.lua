ec87aa04aed39c1f53d9bd5a6379497bac310301
Daniel Zapata <zdaniel4771@gmail.com>
2025-07-26 04:24:46 -0700

    ruff now works along with basedpyright

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -38,6 +38,22 @@ return {
         end,
       })
 
+      -- Disable hover capability from Ruff in favor of basedpyright
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client == nil then
+            return
+          end
+          if client.name == 'ruff' then
+            -- Disable hover in favor of basedpyright
+            client.server_capabilities.hoverProvider = false
+          end
+        end,
+        desc = 'LSP: Disable hover capability from Ruff',
+      })
+
       -- Diagnostic Config
       vim.diagnostic.config({
         severity_sort = true,
@@ -56,7 +72,19 @@ return {
 
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       local servers = {
-        basedpyright = {},
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                typeCheckingMode = "basic",
+              },
+            },
+          },
+        },
+        ruff = {},
         lua_ls = {
           settings = {
             Lua = {
@@ -73,9 +101,10 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua',
+        'ruff',
       })
-      require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
+      require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
       require('mason-lspconfig').setup({
         ensure_installed = {},
         automatic_installation = false,

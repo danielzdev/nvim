3ddc839cece21895c1f4505dadb70bc8682f6361
Daniel Zapata <zdaniel4771@gmail.com>
2025-08-09 19:58:10 -0700

    made map function have the option to take in more than just a desc for opts table, and added some functions for convenience

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
-local map = function(l, r, desc, mode)
-  mode = mode or 'n'
-  vim.keymap.set(mode, l, r, { desc = desc })
+local map = function(lhs, rhs, opts, mode)
+  local o = type(opts) == 'string' and { desc = opts } or (opts or {})
+  vim.keymap.set(mode or 'n', lhs, rhs, o)
 end
 
 map('<C-h>', '<C-w>h', 'Move to left window')
@@ -24,4 +24,6 @@ map(';', ';<c-g>u', 'add breakpoint on pressing ;', 'i')
 map('<S-h>', '<cmd>bprev<cr>', 'Prev buffer')
 map('<S-l>', '<cmd>bnext<cr>', 'Next buffer')
 
-map('<leader>p', [["_dP"]], 'Paste over selection without yanking', 'v')
+map('<leader>p', '"_dP', 'Paste over selection without yanking', 'v')
+map('<leader>d', '"_d', { silent = true, desc = 'Delete without yanking' }, { 'n', 'x' })
+map('<leader>y', '<cmd>silent %y+<CR>', { desc = 'Yank entire buffer to clipboard' })
